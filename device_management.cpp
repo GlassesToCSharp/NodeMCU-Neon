6407f6157595e11bf002a760d7074eddf565a513
Thomas Bartleet <42772107+GlassesToCSharp@users.noreply.github.com>
2024-04-07 16:41:02 +0100

    Reused code to handle name update

--- device_management.cpp
+++ device_management.cpp
@@ -1,13 +1,14 @@
 #include <ArduinoJson.h>
 
 #include "device_management.h"
+#include "server_essentials.h"
 
 // Private variables
-char _deviceName[20] = "Default Name";
+char _deviceName[DEVICE_NAME_MAX_LENGTH] = "Default Name";
+static const char* _jsonKey = "name";
 
 // Private functions
 void _handleNewName();
-void _setName(const char* deviceName);
 
 // Function Definitions
 
@@ -21,22 +22,11 @@ void getDeviceName(char* dst) {
   getTextFromEeprom(address, dst, DEVICE_NAME_MAX_LENGTH);
 }
 
-void _setName(const char* deviceName) {
-  const uint16_t address = getDeviceNameMemoryLocation();
-  writeTextToEeprom(address, deviceName, DEVICE_NAME_MAX_LENGTH);
+static void _onSuccess(JsonDocument* doc) {
+  strncpy(_deviceName, (*doc)["name"], sizeof(_deviceName));
+  writeTextToEeprom(getDeviceNameMemoryLocation(), _deviceName, DEVICE_NAME_MAX_LENGTH);
 }
 
 void _handleNewName() {
-  uint16_t statusCode = failStatusCode;
-  if (server.hasArg("plain")) {
-    String json = server.arg("plain");
-    JsonDocument doc;
-    deserializeJson(doc, json);
-    char deviceName[DEVICE_NAME_MAX_LENGTH];
-    strncpy(deviceName, doc["name"], sizeof(deviceName));
-    statusCode = successEmptyStatusCode;
-    // Save the new name to EEPROM
-    _setName(deviceName);
-  }
-  server.send(statusCode, contentTypePlain, emptyResponse); 
+  handleHttpPost(_jsonKey, _onSuccess);
 }
